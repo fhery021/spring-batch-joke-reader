955c5c4392ac6332c684f2042e19d071d51205c3
FERENC BONDAR <ferenc.bondar@ro.ibm.com>
2020-08-07 13:18:21 +0300

    spring initializer

---
+++ src/main/java/com/home/springbatchjokereader/SpringBatchJokeReaderApplication.java
@@ -0,0 +1,13 @@
+package com.home.springbatchjokereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchJokeReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchJokeReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/springbatchjokereader/SpringBatchJokeReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.springbatchjokereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchJokeReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
